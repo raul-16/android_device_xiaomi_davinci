4819d612a293c21e2327afa9410bcb21dc90ef69
raul16 <cristian.spataru@gmail.com>
2023-02-13 15:27:18 +0200

    davinci: Update vendor security patch level
    Vendor blobs were updated from sweet MIUI V14 Android 13 release SWEETEEAGlobal_V14.0.1.0.TKFEUXM_a95567c393_13.0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,7 +71,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/xiaomi:libudfps_extension.xiaomi
 TARGET_USES_FOD_ZPOS := true
 
 # Vendor security patch level
-VENDOR_SECURITY_PATCH := 2022-12-02
+VENDOR_SECURITY_PATCH := 2023-02-01
 
 # Inherit from proprietary files
 include vendor/xiaomi/davinci/BoardConfigVendor.mk
