303db7f440e15f82ec069ad43fbe67ead369af5d
raul16 <cristian.spataru@gmail.com>
2025-08-04 12:09:36 +0300

    davinci: Update vendor SPL
    vendor blobs updated from Redmi Note 12 Pro 4G (codenamed `sweet_k6a`) OS1.0.19.0.THGMIXM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,7 @@ TARGET_SCREEN_DENSITY := 440
 TARGET_USES_FOD_ZPOS := true
 
 # Vendor security patch level
-VENDOR_SECURITY_PATCH := 2025-02-01
+VENDOR_SECURITY_PATCH := 2025-06-01
 
 # Inherit from proprietary files
 include vendor/xiaomi/davinci/BoardConfigVendor.mk
